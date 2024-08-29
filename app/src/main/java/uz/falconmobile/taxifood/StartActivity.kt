ad49163b2e0c7b216480e03dc48bc1a251013573
yengilmas <lbek458@gmail.com>
2024-08-29 11:53:04 +0500

    First commpit

---
+++ app/src/main/java/uz/falconmobile/taxifood/StartActivity.kt
@@ -0,0 +1,11 @@
+package uz.falconmobile.taxifood
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
